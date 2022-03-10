896a21ac59c569193a3837d279c3f3a10a62467e
Ahmad9023 <g016084j@student.staffs.ac.uk>
2022-03-10 22:50:05 +0000

    tutorial 6 finished

--- marioTest/marioTest.cpp
+++ marioTest/marioTest.cpp
@@ -5,12 +5,16 @@
 #include "constants.h"
 #include "Texture2D.h"
 #include "Commons.h"
+#include "GameScreenManager.h"
 using namespace std;
 
 //Globals
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
 Texture2D* g_texture = nullptr;
+
+GameScreenManager* game_screen_manager;
+Uint32 g_old_time;
 
 //Function prototypes
 bool InitSDL();
@@ -24,6 +28,10 @@ int main(int argc, char* args[])
 {
 	if (InitSDL())
 	{
+		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
+		//set the time
+		g_old_time = SDL_GetTicks();
+
 		//flag to check if we wish to quit
 		bool quit = false;
 
@@ -84,12 +92,12 @@ bool InitSDL()
 	}
 
 	//Load the background texture
-	g_texture = new Texture2D(g_renderer);
+	//g_texture = new Texture2D(g_renderer);
 
-	if (!g_texture->LoadFromFile("Images/test.bmp"))
-	{
-		return false;
-	}
+	//if (!g_texture->LoadFromFile("Images/test.bmp"))
+	//{
+	//	return false;
+	//}
 }
 
 void CLoseSDL()
@@ -109,12 +117,18 @@ void CLoseSDL()
 	g_renderer = nullptr;
 
 	//release the texture
-	delete g_texture;
-	g_texture = nullptr;
+	//delete g_texture;
+	//g_texture = nullptr;
+
+	//destroy the game screen manager
+	delete game_screen_manager;
+	game_screen_manager = nullptr;
 }
 
 bool Update()
 {
+	Uint32 new_time = SDL_GetTicks();
+
 	//Event handler
 	SDL_Event e;
 
@@ -130,6 +144,9 @@ bool Update()
 		break;
 	}
 
+	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
+	g_old_time = new_time;
+
 	return false;
 }
 
@@ -139,7 +156,8 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0XFF, 0XFF);
 	SDL_RenderClear(g_renderer);
 
-	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	//g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	game_screen_manager->Render();
 
 	//update the screen
 	SDL_RenderPresent(g_renderer);
